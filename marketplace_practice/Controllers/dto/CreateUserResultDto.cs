1e5795fa43667287301cce7cdc884c0f73220488
roman123456789-stack <savelijsavinov6@gmail.com>
2025-08-04 14:47:46 +0300

    feat: добавлены конфиги бд для ORM

--- marketplace_practice/Controllers/dto/CreateUserResultDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace marketplace_practice.Controllers.dto
-{
-    public class CreateUserResultDto
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public DateTime ExpiresAt { get; set; }
-        public string RefreshToken { get; set; } = string.Empty;
-    }
-}
--- marketplace_practice/Services/dto/AccessTokenResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace marketplace_practice.Services.dto
-{
-    public class AccessTokenResult
-    {
-        public string AccessToken { get; set; } = string.Empty;
-        public DateTime ExpiresAt { get; set; }
-    }
-}
